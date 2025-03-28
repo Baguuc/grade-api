e00c9938aff3851c37d4d724e7bb2f87d318719f
Jan Tadeusz Żukowski <4886712@teb-gdansk.ad>
2025-03-28 09:28:21 +0100

    update: added database connection

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const express = require('express');
+const pg = require('pg');
+const client = new pg.Client({
+    user: "user17",
+    host: "192.168.0.207",
+    database: "user17_db",
+    password: "6DP4RST8",
+    port: 5432
+});
+
 const app = express()
     .use(express.json())
-    .get('/', (req, res) => {
-        res.send('Hello, World!');
-    })
-    .get('/grades/:student_id/', (req, res) => {
+    .get('/grades/:student_id/', async (req, res) => {
         const studentId = req.params.student_id;
-        console.log("Student ID:", studentId);
+        const sql = "SELECT * FROM grades WHERE student_id = $1";
+        const result = await client.query(sql, [studentId]);
         
+        console.table(result.rows);
+
+        res.send(result.rows);
         res.end();
     })
-    .post('/grades/:student_id/', (req, res) => {
+    .post('/grades/:student_id/', async (req, res) => {
         const studentId = req.params.student_id;
         console.log("Student ID:", studentId);
         console.log('Request Body:', req.body);
 
         res.end();
     })
-    .listen(3000, () => {
+    .listen(3000, async () => {
+        await client.connect();
         console.log('Server is running on port 3000');
+    })
+    .on("close", async () => {
+        await client.end();
     });
